d8faab19384f7803f521b44198203d2a5b934fa6
lampy <lampysprites@hotmail.com>
2026-02-20 13:57:17 +0600

    Update deprecated js syntax in performance timer

--- source/kui/util/ProfilerTimer.hx
+++ source/kui/util/ProfilerTimer.hx
@@ -10,7 +10,7 @@ class ProfilerTimer {
      */
     public static function getTime() {
         #if js
-            return untyped __js__("performance.now() / 1000");
+            return js.Browser.window.performance.now() / 1000;
         #elseif cpp
             return untyped __cpp__("std::chrono::duration_cast<std::chrono::duration<double>>(std::chrono::high_resolution_clock::now().time_since_epoch()).count()");
         #else
